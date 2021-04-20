ae7880491b33f72ae9f361f0a99b1fe72239b707
purandare972 <55679792+purandare972@users.noreply.github.com>
2021-04-20 20:19:56 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,54 @@
+<!-- navbar --> 
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <style type="text/css">
+
+        body{margin-top: 15px;
+      
+        }
+       
+          .btn {
+                  border: 2px solid black;
+                  background-color: #ffff99;
+                  color: black;
+                  padding: 14px 28px;
+                  font-size: 16px;
+                  cursor: pointer;
+                  margin: 5px;
+                }
+            
+            .df:hover {
+        color: white;
+        font-size: 16px;
+    background-color: #9dc5c3;
+background-image: linear-gradient(315deg, #9dc5c3 0%, #5e5c5c 74%);
+
+}
+
+        </style>
+      </head>
+      <body>
+      
+      <nav  class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php"></a>
+      
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="btn df" class="nav-link" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="btn df" class="nav-link" href="transfermoney.php">View Users</a>
+              </li>
+              <li class="nav-item">
+                <a class="btn df" class="nav-link" href="transfermoney.php">Transfer Money</a>
+              </li>
+              <li class="nav-item">
+                <a class="btn df" class="nav-link" href="transactionhistory.php">Transfer History</a>
+              </li>
+              
+          </div>
+       </nav>
+     </body>
+     </html>
